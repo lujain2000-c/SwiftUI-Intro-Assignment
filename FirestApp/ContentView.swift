30fa11c99658abe96199e14cd4350cf1f2d17e83
LUJAIN ALKANHAL <lujain.i.k20@gmail.com>
2023-05-15 19:16:03 +0300

    Initial Commit

---
+++ FirestApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FirestApp
+//
+//  Created by لجين إبراهيم الكنهل on 25/10/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirestApp/FirestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirestAppApp.swift
+//  FirestApp
+//
+//  Created by لجين إبراهيم الكنهل on 25/10/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct FirestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
