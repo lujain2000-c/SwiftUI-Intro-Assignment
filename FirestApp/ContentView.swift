0b4a5fc6e98e13f4d2e16afb6e5f91b1583235c4
LUJAIN ALKANHAL <lujain.i.k20@gmail.com>
2023-05-16 00:46:27 +0300

    SwiftUI-Intro-Assignment

--- FirestApp/ContentView.swift
+++ FirestApp/ContentView.swift
@@ -9,12 +9,66 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .center,spacing: 200){
+            Text("What's New in Shortcute")
+                .font(.largeTitle)
+                
+                .bold()
+                
+                
+            
+            VStack(alignment: .leading, spacing: 10) {
+                HStack{
+                    Image(systemName: "square.and.pencil")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(.blue)
+                    VStack(alignment: .leading){
+                        Text("Gallery")
+                        
+                            .font(.title3)
+                            .bold()
+                        
+                        Text("Git Suggest shortcuts from your favorite app")
+                            .foregroundColor(.gray)
+                            .font(.callout)
+                    }
+                    
+                }
+                
+                HStack{
+                    Image(systemName: "mic")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(.blue)
+                    
+                    VStack(alignment: .leading){
+                        Text("Siri")
+                            .font(.title3)
+                            .bold()
+                        Text("Run any shortcut by using your voice")
+                            .foregroundColor(.gray)
+                            .font(.callout)
+                    }
+                    
+                    
+                }
+            }
+            
+            Button("continue") {
+                      
+                   }
+            .padding(.horizontal, 140)
+
+                        .padding(.vertical, 15)
+
+                        .foregroundColor(.white)
+                        .background(.blue)
+                        .cornerRadius(10)
         }
+        .frame(maxWidth: .infinity,
+               maxHeight: .infinity
+               )
         .padding()
     }
 }
